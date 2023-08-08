d1970eadaa42ef8a5b10c946fa0297de22bd5f39
davlondev <david.wip.long@gmail.com>
2023-08-08 17:57:33 +0800

    add arguments and update readme

--- README.md
+++ README.md
@@ -1,16 +1,30 @@
-<h1>Bigram and trigram model</h1>
+<h1>Name generation with N-gram probabilities</h1>
 
-<p>Basic bigram and trigram <a href="https://en.wikipedia.org/wiki/N-gram">(read...)</a> model written in Python using only numpy.</p>
+<p>Name generator using <a href="https://en.wikipedia.org/wiki/N-gram">N-gram</a> probabilities written in Python using only numpy.</p>
 
 <p>Based on <a href="https://github.com/karpathy/makemore" target="_blank">Karpathy's</a> bigram model shown in his makemore lectures.</p>
+
+<p>Example usage:</p>
+<pre><p>python3 ngram.py -n6 -N30</p></pre>
+<ul>
+    <li>-n6 (6-gram model)</li>
+    <li>-N30 (generate 30 names)</li>
+</ul>
+
 <br>
-<img src="preview.png" width="250px">
+
+<img src="preview.png" width="400px">
+
 <br>
+
 <p>Dependencies:</p>
 <ul>
-    <li>Numpy</li>
+    <li>numpy (arrays, multinomial)</li>
+    <li>tqdm (loading bars)</li>
 </ul>
+
 <br>
+
 <p>Todo:</p>
 <ul>
     <s><li>Automatic <b>n-gram</b> probability distributions generator</li></s>
--- ngram.py
+++ ngram.py
@@ -1,7 +1,8 @@
-#import argparse
+import argparse
 from itertools import islice
 import numpy as np
 from numpy.random import multinomial
+from tqdm import tqdm
 
 def prep_names(file, n=1):
   words = []
@@ -14,8 +15,14 @@ def prep_names(file, n=1):
 if __name__ == "__main__":
   np.set_printoptions(suppress=True)
   #g = default_rng(1337)
+  parser = argparse.ArgumentParser(description="N-gram model for name generation")
+  parser.add_argument("-n", "--ngram", type=int, required=True, help="Value of n for n-grams")
+  parser.add_argument("-N", "--num_names", type=int, required=True, help="Value of n for n-grams")
 
-  n = int(input("N grams: "))
+  args = parser.parse_args()
+  n = args.ngram
+  k = args.num_names
+
   assert n >= 2
 
   names = prep_names("datasets/names.txt", n)
@@ -25,7 +32,7 @@ if __name__ == "__main__":
 
   ngrams = np.zeros((27,)*n)
 
-  for name in names:
+  for name in tqdm(names, desc=f"Counting {n}-grams"):
     for group in zip(*(islice(name, i, None) for i in range(n))):
       c_values = group
       i_values = np.array([stoi[c] for c in c_values])
@@ -33,12 +40,11 @@ if __name__ == "__main__":
       val = tuple(i_values[n-1:])
       ngrams[pos][val] += 1
 
-  for idx in np.ndindex(*ngrams.shape[:-1]):
+  its = np.prod(ngrams.shape[:-1])
+  for idx in tqdm(np.ndindex(*ngrams.shape[:-1]), total=its, desc="Calculating probabilities"):
     s = ngrams[idx].sum()
     if s != 0.0:
       ngrams[idx] = ngrams[idx] / s
-
-  k = int(input("Number of names to generate: "))
 
   stoi2 = lambda aa: tuple([stoi[a] for a in aa])
 
